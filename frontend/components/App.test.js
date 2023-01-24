9e8c2ead3677c6c321354cad6335f66920baca11
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-01-23 19:00:47 -0500

    wrote tests for AppClass.js

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,38 @@
+import React from 'react';
+import AppClass from './AppClass';
+import { render, screen, fireEvent } from '@testing-library/react';
+
 // Write your tests here
 test('sanity', () => {
   expect(true).toBe(false)
+})
+
+test('renders without errors', () => {
+  render (<AppClass />);
+})
+
+test('direction buttons render', () => {
+  render (<AppClass />);
+
+  const leftButton = screen.queryByText('LEFT');
+  const rightButton = screen.queryByText('RIGHT');
+  const upButton = screen.queryByText('UP');
+  const downButton = screen.queryByText('DOWN');
+  const resetButton = screen.queryByText('reset');
+
+  expect(leftButton).toBeInTheDocument();
+  expect(rightButton).toBeInTheDocument();
+  expect(upButton).toBeInTheDocument();
+  expect(downButton).toBeInTheDocument();
+  expect(resetButton).toBeInTheDocument();
+})
+
+test('can type email in input', () => {
+  render(<AppClass />);
+
+  const formInput = screen.getByRole('email');
+
+  fireEvent.change(formInput, 'v@sealsnutt.com');
+
+  expect(formInput).toHaveValue('v@sealsnutt.com');
 })
